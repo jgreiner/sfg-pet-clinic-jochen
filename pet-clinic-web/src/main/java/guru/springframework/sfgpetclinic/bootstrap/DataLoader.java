2eba12d6d5c4e6e2f531dfedb9ca6267ba677f0b
Jochen Greiner <jochen@greiner.de>
2020-06-03 19:20:52 +0200

    closes #23

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -26,28 +26,28 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("################################### hallo");
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
+
         owner1.setFirstName("Jochen");
         owner1.setLastName("Greiner");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
+
         owner2.setFirstName("Jochen2");
         owner2.setLastName("Greiner2");
 
         ownerService.save(owner2);
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
+
         vet1.setFirstName("Sam");
         vet1.setLastName("Vettr");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
+
         vet2.setFirstName("Sam2");
         vet2.setLastName("Vettr2");
 
