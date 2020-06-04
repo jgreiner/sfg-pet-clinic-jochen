b84a5d4dc0f17d7653d63144311fe898d4b15808
Jochen Greiner <jochen@greiner.de>
2020-06-04 12:20:29 +0200

    closes #28

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -11,4 +11,10 @@ import java.util.Set;
 public class Owner extends Person {
 
     private Set<Pet> pets;
+
+    private String address;
+
+    private String city;
+
+    private String telephone;
 }
